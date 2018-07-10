07c38244c760cf02e8d755a7fdca9c0aa857ecac
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-10 10:25:52 +0100

    Tests

--- RedPitaya/Examples/C/Angel.c
+++ RedPitaya/Examples/C/Angel.c
@@ -40,7 +40,7 @@ int main(int argc, char **argv){
         rp_AcqStart();
 	sleep(1);
         rp_AcqSetTriggerSrc(RP_TRIG_SRC_DISABLED);
-        rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
+        //rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
 
         /*while(1){
                 rp_AcqGetTriggerState(&state);
