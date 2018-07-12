f2ff44f8cb14a6cfb395e7abc6046fd7c0993382
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-12 14:08:55 +0100

    Tests

--- RedPitaya/Examples/C/Angel.c
+++ RedPitaya/Examples/C/Angel.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include "redpitaya/rp.h"
 
-#define NSMP 300000
+#define NSMP 19531250
 typedef struct node
 {
     float data;
@@ -103,7 +103,7 @@ int main(int argc, char **argv){
 	float freq=0;
         rp_AcqReset();
 	rp_AcqSetArmKeep(true);
-        rp_AcqSetDecimation(RP_DEC_8);
+        rp_AcqSetDecimation(RP_DEC_64);
         rp_AcqSetTriggerLevel(RP_CH_1, 0);
         rp_AcqSetTriggerDelay(0);
 
