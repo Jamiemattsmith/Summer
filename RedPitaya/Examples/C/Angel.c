7b641ab06b2d6d6e28883eaaa7b48888b0e76cbf
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-12 09:22:36 +0100

    Tests

--- RedPitaya/Examples/C/Angel.c
+++ RedPitaya/Examples/C/Angel.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv){
 	float freq=0;
         rp_AcqReset();
 	rp_AcqSetArmKeep(true);
-        rp_AcqSetDecimation(RP_DEC_1024);
+        rp_AcqSetDecimation(RP_DEC_64);
         rp_AcqSetTriggerLevel(RP_CH_1, 0);
         rp_AcqSetTriggerDelay(0);
 
@@ -43,18 +43,15 @@ int main(int argc, char **argv){
         rp_AcqSetTriggerSrc(RP_TRIG_SRC_DISABLED);
 
 	rp_AcqGetWritePointer(&posnow);
-int j;
-int i;
+	int j;
+	int i;
 
-for(j = 0; j < 30000; j++){
-	buff_size=16384;
-	posold=posnow;
-	rp_AcqGetWritePointer(&posnow);
-	//if(posold!=posnow){posold++;}
-	//printf("%d\n",posold);
-	//printf("%d\n",posnow); 
+	for(j = 0; j < 30; j++){
+		buff_size=16384;
+		posold=posnow;
+		rp_AcqGetWritePointer(&posnow);
              
-       rp_AcqGetDataPosV(RP_CH_1,posold,posnow, buff, &buff_size);
+        	rp_AcqGetDataPosV(RP_CH_1,posold,posnow, buff, &buff_size);
         	for(i = 1; i < buff_size; i++){
                 	//printf("%f\n", buff[i]);
 			fprintf(f, "%f\n", buff[i]);
