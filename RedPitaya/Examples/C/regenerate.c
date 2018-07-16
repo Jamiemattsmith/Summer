c84e1cd56983a6e13e097f241e11570de43e2984
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-16 09:26:39 +0100

    Tests

--- RedPitaya/Examples/C/regenerate.c
+++ RedPitaya/Examples/C/regenerate.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv){
 
 	int i;
-	int buff_size = 30000;
+	int buff_size = 16385;
 
 	/* Print error, if rp_Init() function failed */
 	if(rp_Init() != RP_OK){
@@ -19,7 +19,6 @@ int main(int argc, char **argv){
 
 	float *t = (float *)malloc(buff_size * sizeof(float));
 	float *x = (float *)malloc(buff_size * sizeof(float));
-	float *y = (float *)malloc(buff_size * sizeof(float));
 
 	for(i = 1; i < buff_size; i++){
 		t[i] = (2 * M_PI) / buff_size * i;
@@ -27,26 +26,18 @@ int main(int argc, char **argv){
 
 	for (int i = 0; i < buff_size; ++i){
 		x[i] = sin(t[i]) + ((1.0/3.0) * sin(t[i] * 3));
-		y[i] = (1.0/2.0) * sin(t[i]) + (1.0/4.0) * sin(t[i] * 4);
 	}
 
 	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_ARBITRARY);
-	rp_GenWaveform(RP_CH_2, RP_WAVEFORM_ARBITRARY);
 
 	rp_GenArbWaveform(RP_CH_1, x, buff_size);
-	rp_GenArbWaveform(RP_CH_2, y, buff_size);
 
 	rp_GenAmp(RP_CH_1, 0.7);
-	rp_GenAmp(RP_CH_2, 1.0);
 
 	rp_GenFreq(RP_CH_1, 4000.0);
-	rp_GenFreq(RP_CH_2, 4000.0);
 
 	rp_GenOutEnable(RP_CH_1);
-	rp_GenOutEnable(RP_CH_2);
-
 	/* Releasing resources */
-	free(y);
 	free(x);
 	free(t);
 	rp_Release();
