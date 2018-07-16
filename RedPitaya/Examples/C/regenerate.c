298a9460df22f35dcaddf522da67adb53bcf0570
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-16 10:16:51 +0100

    Tests

--- RedPitaya/Examples/C/regenerate.c
+++ RedPitaya/Examples/C/regenerate.c
@@ -27,10 +27,11 @@ int main(int argc, char **argv){
 	for (int i = 0; i < buff_size; ++i){
 		x[i] = sin(t[i]);
 	}
-
+	float x1[16384];
+	memcpy(x1,x,16384*sizeof(float));
 	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_ARBITRARY);
 
-	rp_GenArbWaveform(RP_CH_1, x, buff_size);
+	rp_GenArbWaveform(RP_CH_1, x1, 16384);
 	rp_GenMode(RP_CH_1, RP_GEN_MODE_STREAM);
 	rp_GenAmp(RP_CH_1, 0.7);
 
