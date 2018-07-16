cb9788142d14c2043124ff908991155136c55a18
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-16 15:32:48 +0100

    Experimenting with these libraries to create new functions

--- RedPitaya/Examples/C/regenerate.c
+++ RedPitaya/Examples/C/regenerate.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv){
 
 	int i;
-	int buff_size = 32768;
+	int buff_size = 16384;
 	uint32_t posnow = 0;
 
 	/* Print error, if rp_Init() function failed */
@@ -19,7 +19,6 @@ int main(int argc, char **argv){
 	}
 
 	float *t = (float *)malloc(buff_size * sizeof(float));
-	float *x = (float *)malloc(buff_size * sizeof(float));
 
 	for(i = 1; i < buff_size; i++){
 		t[i] = (2 * M_PI) / buff_size * i;
@@ -31,11 +30,9 @@ int main(int argc, char **argv){
 	float x1[16384];
 	float x2[16384];
 	memcpy(x1,x,16384*sizeof(float));
-	memcpy(x2,&x[16384],16384*sizeof(float));
 	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_ARBITRARY);
 	rp_GenMode(RP_CH_1, RP_GEN_MODE_CONTINUOUS);
 	rp_GenArbWaveform(RP_CH_1, x1, 16384);
-	rp_GenArbWaveform(RP_CH_1, x2, 16384);
 	rp_GenAmp(RP_CH_1, 0.7);
 
 	rp_GenFreq(RP_CH_1, 4000.0);
