0985010cc54114c6f1935effeeb81781f41539df
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-17 12:49:06 +0100

    Tests

--- RedPitaya/Examples/C/regenerate.c
+++ RedPitaya/Examples/C/regenerate.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include "redpitaya/rp3.h"
+#include "redpitaya/rp.h"
 #define M_PI 3.14159265358979323846
 
 int main(int argc, char **argv){
