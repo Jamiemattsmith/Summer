428b1c9682da700ba8b6370b6e2686e1186c838e
jamiemattsmith <jamie.smith@ukaea.uk>
2018-07-30 16:30:32 +0100

    Tests

--- RedPitaya/fpga/prj/classic/rtl/red_pitaya_top.sv
+++ RedPitaya/fpga/prj/classic/rtl/red_pitaya_top.sv
@@ -370,7 +370,7 @@ end
   m_avg[i]<=asg_dat[0];
   dac_dat_b <= {dac_b[14-1], ~dac_b[14-2:0]};
   //dac_dat_b <= {dac_b[14-1], ~dac_b[14-2:0]};
-  dac_dat_a <= m_avg_sum[19:6];
+  dac_dat_a <= {m_avg_sum[19], ~m_avg_sum[18:6]};
 end
 
 // DDR outputs
